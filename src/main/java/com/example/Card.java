c3754d7cd541b4cd6a699103f8e327541f92e442
Leandro Gay <leandroakk@gmail.com>
2025-02-20 14:54:32 +0800

    added Card.java

---
+++ src/main/java/com/example/Card.java
@@ -0,0 +1,24 @@
+class Card {
+    enum Color { RED, BLUE, GREEN, YELLOW, PURPLE, BLACK }
+    
+    private final Color color;
+    private final int value;
+
+    public Card(Color color, int value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + value;
+    }
+}
