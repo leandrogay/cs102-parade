06d2bfddfcd9bb8af93755e184b5c7bcd9e8c0cf
Kayweesim <kaywee.sim.2024@smu.edu.sg>
2025-02-20 15:07:29 +0800

    added main method for app

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of players: ");
+        int playerCount = scanner.nextInt();
+        // Game game = new Game(playerCount);
+        // game.start();
+    }
+}
