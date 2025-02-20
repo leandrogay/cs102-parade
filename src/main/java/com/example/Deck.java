a8f3ec317b0c54f46dc41d670343bc1385897a67
Leandro Gay <leandroakk@gmail.com>
2025-02-20 15:04:25 +0800

    added code to deck

--- src/main/java/com/example/Deck.java
+++ src/main/java/com/example/Deck.java
@@ -1,3 +1,19 @@
+import java.util.Collections;
+import java.util.Stack;
+
 public class Deck {
-    
+    private final Stack<Card> cards = new Stack<>();
+
+    public Deck() {
+        for (Card.Color color : Card.Color.values()) {
+            for (int value = 0; value <= 10; value++) {
+                cards.add(new Card(color, value));
+            }
+        }
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        return cards.isEmpty() ? null : cards.pop();
+    }
 }
