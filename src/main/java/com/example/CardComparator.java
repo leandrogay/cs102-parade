1f1fad380820edc710c30fb6e6f3882ae9b48a18
leandrogay <leandroakk@gmail.com>
2025-03-05 00:21:47 +0800

    added annotations for comparator class

--- src/main/java/com/example/CardComparator.java
+++ src/main/java/com/example/CardComparator.java
@@ -3,12 +3,14 @@ import java.util.Comparator;
 public class CardComparator implements Comparator<Card> {
 
     public int compare(Card card1, Card card2) {
+        // compares based on their ordinal values declared in enum 
         int cmp = card1.getColor().compareTo(card2.getColor());
 
+        // returns if the colours of the card objects are not the same 
         if (cmp != 0) {
             return cmp;
         }
-
+        // returns by ascending order
         return card1.getValue() - card2.getValue();
     }
 }
