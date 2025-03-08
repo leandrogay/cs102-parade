735640e29e051074d9c2e62a02f15f28575bfb87
leandrogay <leandroakk@gmail.com>
2025-03-09 03:09:33 +0800

    created Scoreboard reader and writer

---
+++ src/main/java/com/example/Scoreboard.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Scoreboard {
+    public void readScore(String pathname) {
+        try (Scanner sc = new Scanner(new File(pathname))) {
+            sc.useDelimiter(",|\r\n|\n"); // ",|\r\n|\n" <- regular expression
+
+            while (sc.hasNext()) {
+                String name = sc.next(); // Read name as String
+                double score = sc.nextDouble(); // Read score as double
+                LocalDate date = LocalDate.parse(sc.next()); // Read and parse date
+
+                System.out.println("Name: " + name);
+                System.out.println("Score: " + score);
+                System.out.println("Date: " + date);
+                System.out.println("======================");
+                
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    public void writeScore(String pathname, Player player) {
+        try (PrintStream out = new PrintStream(new FileOutputStream(new File(pathname), true))) {
+            out.println(player.getName() + "," + player.getScore() + "," + LocalDate.now()); // e.g. format -> Leandro,2000.69,2025-03-09
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
