4efb51829a441e47d46567dee9a7e4f336a588ef
leandrogay <leandroakk@gmail.com>
2025-03-12 18:18:18 +0800

    Added tempoary test cases to display table and playerhands

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,8 +7,21 @@ public class App {
         int playerCount = sc.nextInt();
         if (playerCount >= 2 && playerCount <= 6) { // exception needed temp for now
             Game game = new Game(playerCount);
+
+            System.out.println("Display Table");
+            System.out.println("=============");
             for (Card card : game.getParade()) {
                 System.out.println(card);
+            }
+            System.out.println("=============");
+
+            for (Player player : game.getPlayers()){
+                System.out.println(player.getName());
+                System.out.println("=============");
+                for (Card card : player.getPlayerHand()) {
+                    System.out.println(card);
+                }
+                System.out.println("=============");
             }
         }
     }
