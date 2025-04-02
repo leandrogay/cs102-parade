7089753eeae57a1bed7f827a8d4ed0c9b78c818d
Leandro Gay <leandroakk@gmail.com>
2025-04-02 10:24:53 +0800

    Removed redundant comments

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,7 +7,7 @@ public class App {
         int playerCount = 0;
 
         boolean isValidPlayerCount = false;
-        
+
         while (!isValidPlayerCount) {
             try {
                 System.out.print("Enter number of players: ");
@@ -22,42 +22,27 @@ public class App {
             } catch (InputMismatchException e) {
                 System.out.println("Invalid Input");
                 sc.nextLine();
-            }   
+            }
         }
-
 
         System.out.println();
 
-        if (playerCount >= 2 && playerCount <= 6) { 
+        if (playerCount >= 2 && playerCount <= 6) {
             Game game = new Game(playerCount);
             int cheat = 2;
-            
+
             while (cheat > 0) {
-            // while (!game.checkLastRound()) {
-                game.conductRound(); // conduct round while not last round 
+                // while (!game.checkLastRound()) {
+                game.conductRound(); // conduct round while not last round
                 cheat--;
             }
 
-            // game.conductScoringRound(); // for scoring
-
-            // one more round
-            // 1. Notify that it is the last round - ""
-            // 2. repeat contents of for loop 
-            // System.out.println("=====LAST ROUND=====");
             DisplayUtility.printDivider("LAST ROUND");
             game.conductRound();
-            //this is the round where the player chooses to discard 2, and place 2
             game.conductScoringRound();
 
-            //tabulate scores and stuff and print winner
-
-            //print winner
             ArrayList<Player> players = game.getPlayers();
-
-            //print out each collection and hand
             DisplayUtility.printDivider("SCORES");
-            // System.out.println("=====SCORES=====");
-
 
             for (Player p : players) {
                 System.out.println("score for " + p.getName() + " is " + p.getScore());
@@ -66,12 +51,11 @@ public class App {
 
             ArrayList<Player> winners = game.getWinner();
             DisplayUtility.printDivider("WINNERS");
-            // System.out.println("=====WINNERS=====");
             System.out.println("WINNERS: ");
 
             for (Player winner : winners) {
                 System.out.println(winner.getName());
-            }            
-        }  
+            }
+        }
     }
 }
